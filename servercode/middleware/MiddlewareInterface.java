03a9fc2b52a08e9f954c3150caad63cad896156e
Jeremie <jpoisson@magicGrilledCheese.local>
2017-09-25 20:46:22 -0400

    added middleware server

---
+++ servercode/middleware/MiddlewareInterface.java
@@ -0,0 +1,178 @@
+package middleware;
+
+import ResInterface.ResourceManager;
+import middleware.resource_managers.ResourceManagerTypes;
+
+import java.rmi.RemoteException;
+import java.util.Vector;
+
+/**
+ * Created by jpoisson on 2017-09-25.
+ */
+public class MiddlewareInterface implements ResourceManager {
+    private MiddlewareServer middleware;
+
+    MiddlewareInterface(MiddlewareServer serverInstance) {
+        this.middleware = serverInstance;
+    }
+
+    @Override
+    public boolean addFlight(int id, int flightNum, int flightSeats, int flightPrice) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.FLIGHTS_ONLY)
+                .getResourceManager()
+                .addFlight(id, flightNum, flightSeats, flightPrice);
+    }
+
+    @Override
+    public boolean addCars(int id, String location, int numCars, int price) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.CARS_ONLY)
+                .getResourceManager()
+                .addCars(id, location, numCars, price);
+    }
+
+    @Override
+    public boolean addRooms(int id, String location, int numRooms, int price) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.ROOMS_ONLY)
+                .getResourceManager()
+                .addRooms(id, location, numRooms, price);
+    }
+
+    @Override
+    public int newCustomer(int id) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.OTHERS)
+                .getResourceManager()
+                .newCustomer(id);
+    }
+
+    @Override
+    public boolean newCustomer(int id, int cid) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.OTHERS)
+                .getResourceManager()
+                .newCustomer(id, cid);
+    }
+
+    @Override
+    public boolean deleteFlight(int id, int flightNum) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.FLIGHTS_ONLY)
+                .getResourceManager()
+                .deleteFlight(id, flightNum);
+    }
+
+    @Override
+    public boolean deleteCars(int id, String location) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.CARS_ONLY)
+                .getResourceManager()
+                .deleteCars(id, location);
+    }
+
+    @Override
+    public boolean deleteRooms(int id, String location) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.ROOMS_ONLY)
+                .getResourceManager()
+                .deleteRooms(id, location);
+    }
+
+    @Override
+    public boolean deleteCustomer(int id, int customer) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.OTHERS)
+                .getResourceManager()
+                .deleteCustomer(id, customer);
+    }
+
+    @Override
+    public int queryFlight(int id, int flightNumber) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.FLIGHTS_ONLY)
+                .getResourceManager()
+                .queryFlight(id, flightNumber);
+    }
+
+    @Override
+    public int queryCars(int id, String location) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.CARS_ONLY)
+                .getResourceManager()
+                .queryCars(id, location);
+    }
+
+    @Override
+    public int queryRooms(int id, String location) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.ROOMS_ONLY)
+                .getResourceManager()
+                .queryRooms(id, location);
+    }
+
+    @Override
+    public String queryCustomerInfo(int id, int customer) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.OTHERS)
+                .getResourceManager()
+                .queryCustomerInfo(id, customer);
+    }
+
+    @Override
+    public int queryFlightPrice(int id, int flightNumber) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.FLIGHTS_ONLY)
+                .getResourceManager()
+                .queryFlightPrice(id, flightNumber);
+    }
+
+    @Override
+    public int queryCarsPrice(int id, String location) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.CARS_ONLY)
+                .getResourceManager()
+                .queryCarsPrice(id, location);
+    }
+
+    @Override
+    public int queryRoomsPrice(int id, String location) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.ROOMS_ONLY)
+                .getResourceManager()
+                .queryRoomsPrice(id, location);
+    }
+
+    @Override
+    public boolean reserveFlight(int id, int customer, int flightNumber) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.FLIGHTS_ONLY)
+                .getResourceManager()
+                .reserveFlight(id, customer, flightNumber);
+    }
+
+    @Override
+    public boolean reserveCar(int id, int customer, String location) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.CARS_ONLY)
+                .getResourceManager()
+                .reserveCar(id, customer, location);
+    }
+
+    @Override
+    public boolean reserveRoom(int id, int customer, String locationd) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.ROOMS_ONLY)
+                .getResourceManager()
+                .reserveRoom(id, customer, locationd);
+    }
+
+    @Override
+    public boolean itinerary(int id, int customer, Vector flightNumbers, String location, boolean Car, boolean Room) throws RemoteException {
+        return this.middleware
+                .getRemoteResourceManagerForType(ResourceManagerTypes.OTHERS)
+                .getResourceManager()
+                .itinerary(id, customer, flightNumbers, location, Car, Room);
+    }
+}
--- servercode/middleware/MiddlewareServer.java
+++ servercode/middleware/MiddlewareServer.java
@@ -1,10 +1,16 @@
 package middleware;
 
+import ResInterface.ResourceManager;
 import middleware.exceptions.MiddlewareBaseException;
 import middleware.resource_managers.AbstractRemoteResourceManager;
 import middleware.resource_managers.RemoteResourceManagerFactory;
 import middleware.resource_managers.ResourceManagerTypes;
 
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.LinkedList;
@@ -15,14 +21,19 @@ import java.util.List;
  */
 public class MiddlewareServer {
     private static final RemoteResourceManagerFactory remoteRMFactory = new RemoteResourceManagerFactory();
+    private static final String rmiServer = "localhost";
+    private static final String rmiMiddlewareKey = "Group33Middleware";
+    private static final int rmiPort = 8080;
 
     private List<String> availableRMs;
     private Hashtable<ResourceManagerTypes, AbstractRemoteResourceManager> remoteResourceManagers;
+    private MiddlewareInterface middlewareInterface;
 
     public MiddlewareServer(String... availableRMIs) {
         this.availableRMs = Arrays.asList(availableRMIs);
 
         this.initializeRemoteResourceManagers();
+        this.initializeRMIServer();
     }
 
     private void initializeRemoteResourceManagers() {
@@ -39,6 +50,29 @@ public class MiddlewareServer {
         this.remoteResourceManagers.put(ResourceManagerTypes.FLIGHTS_ONLY, remoteRMFactory.createRemoteResourceManager(rmStrs.pop()));
         this.remoteResourceManagers.put(ResourceManagerTypes.ROOMS_ONLY, remoteRMFactory.createRemoteResourceManager(rmStrs.pop()));
         this.remoteResourceManagers.put(ResourceManagerTypes.OTHERS, remoteRMFactory.createRemoteResourceManager(rmStrs.pop()));
+    }
+
+    private void initializeRMIServer() {
+        this.middlewareInterface = new MiddlewareInterface(this);
+        try {
+            ResourceManager rm = (ResourceManager) UnicastRemoteObject.exportObject(this.middlewareInterface, 0);
+            Registry rmiRegistry = LocateRegistry.getRegistry(rmiPort);
+            rmiRegistry.rebind(rmiMiddlewareKey, rm);
+
+            if (System.getSecurityManager() == null) {
+                System.setSecurityManager(new RMISecurityManager());
+            }
+        } catch (RemoteException e) {
+            throw new MiddlewareBaseException("Unable to initialize the middleware RMI Server properly.", e);
+        }
+    }
+
+    public AbstractRemoteResourceManager getRemoteResourceManagerForType(ResourceManagerTypes type) {
+        if (!this.remoteResourceManagers.containsKey(type)) {
+            throw new MiddlewareBaseException("No remote resource manager for type " + type.toString());
+        }
+
+        return this.remoteResourceManagers.get(type);
     }
 
 
