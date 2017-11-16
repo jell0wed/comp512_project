810557a0558d79e88ec30d060ba53cac5c0db52e
Jeremie Poisson <jeremie@weboxigen.com>
2017-11-16 14:03:33 -0500

    fix

--- servercode/bin/startMiddleware.sh
+++ servercode/bin/startMiddleware.sh
@@ -2,4 +2,4 @@
 
 export CLASSPATH=$(echo "$(pwd)/../out_gradle/")
 
-java -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:/Users/jpoisson/Desktop/comp512/servercode/ middleware.impl.rmi.MiddlewareRMIServer
+java -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:/home/2015/jpoiss12/comp512_project/servercode/ middleware.impl.rmi.MiddlewareRMIServer
